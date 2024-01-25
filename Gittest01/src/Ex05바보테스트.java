60fe301a9734d1e6c965151e3e89ef4d3b80665e
smhrd <smhrd@211.33.239.178>
2024-01-25 12:14:07 +0900

    바보테스트

---
+++ Gittest01/src/Ex05바보테스트.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex05바보테스트 {
+
+	public static void main(String[] args) {
+		Random ran=new Random();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("정수를 입력해주세요");
+		int n=sc.nextInt();
+		int a=ran.nextInt(1);
+		if(n%2==a) {System.out.println("천재입니다");}
+		else {System.out.println("바보입니다");}
+		//실행 해주세요
+
+	}
+
+}
