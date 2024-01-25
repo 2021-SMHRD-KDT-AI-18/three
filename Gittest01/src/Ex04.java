765f3dbf064035f17371f8fa621d0353af6e6934
smhrd <smhrd@59.0.129.246>
2024-01-25 12:00:01 +0900

    Ex04 정말정말완료본

--- Gittest01/src/Ex04.java
+++ Gittest01/src/Ex04.java
@@ -2,7 +2,11 @@
 public class Ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.print("영빈이는 바보이다.");
+	System.out.print("연주 또한 바보이다.");
+	System.out.print("그러나 종우는 천재이다.");
+		
+	
 
 	}
 
