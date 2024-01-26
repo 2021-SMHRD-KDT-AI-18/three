6091417c7b30da0e414e4b2a757924fbd562ef9e
smhrd <smhrd@211.33.239.178>
2024-01-26 14:56:39 +0900

    연결

--- Gittest01/src/blackAndWhite/view.java
+++ Gittest01/src/blackAndWhite/view.java
@@ -38,7 +38,6 @@ public class view {
 		
 		
 		
-		
 		//게임시작
 		while(nextStage<=3) {
 			if(nextStage==0) {System.out.println("16강");}
@@ -55,8 +54,12 @@ public class view {
 			playerWin=0;
 		while(count<9) {
 			if(nextStage==0) {com=comNumber[count];}
-			else if(nextStage==1) {com=comNumber[count];}
-			else if(nextStage==2) {com=comNumber[count];}
+			else if(nextStage==1) {com=comNumber[count]+1;}
+			else if(nextStage==2) {
+				if(count%3==0) {
+					com=comNumber[count]+5;
+					}else {com=comNumber[count];}
+			}
 			else if(nextStage==3) {com=comNumber[count];}
 			
 			if(com%2==0) {
