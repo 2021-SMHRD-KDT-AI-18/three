5022c296eaa4ff4fce5ea4f1a24d2624086519b7
smhrd <smhrd@59.0.129.246>
2024-01-29 10:22:30 +0900

    MVC 편집본 오전 10:21

--- Gittest01/src/mvc/GameView.java
+++ Gittest01/src/mvc/GameView.java
@@ -2,23 +2,20 @@ package mvc;
 
 import java.util.Scanner;
 
-public class View {
-    private Scanner sc;
+public class GameView {
+	private Scanner sc;
 
-    public View() {
+	public void View() {
         sc = new Scanner(System.in);
     }
 
-    public void displayMessage(String message) {
-        System.out.println(message);
-    }
+	public void displayMessage(String message) {
+		System.out.println(message);
+	}
 
-    public int getPlayerInput() {
-        return sc.nextInt();
-    }
+	public int getPlayerInput() {
+		return sc.nextInt();
+	}
 
-    // Add other methods for displaying game information
+	// Add other methods for displaying game information
 }
-
-}
-
